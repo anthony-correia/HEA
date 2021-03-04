6d1699c397a29d68cf74433a8b91a09786781cdf
anthony-correia <anthony.correia@cern.ch>
2021-03-04 14:23:15 +0000

    Correct conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-autodoc_mock_imports = ["root_pandas", "zfit"]
+autodoc_mock_imports = ["root_pandas", "zfit", "hep_ml"]
 
 
 latex_engine = 'pdflatex'
